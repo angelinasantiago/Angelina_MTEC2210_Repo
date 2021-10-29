80191cb65f8876884342c00fb06016c1f94d8c12
angelinasantiago <77749659+angelinasantiago@users.noreply.github.com>
2021-10-28 21:04:41 -0400

    added sprites

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -6,10 +6,13 @@ public class PlayerMovement : MonoBehaviour
 {
     public float speed;
     public GameObject snorlaxPrefab;
+    public Sprite originalSprite;
+    public Sprite secondSprite;
+    private SpriteRenderer sr;
     // Start is called before the first frame update
     void Start()
     {
-        
+        sr = GetComponent<SpriteRenderer>(); 
     }
 
     // Update is called once per frame
@@ -18,19 +21,31 @@ public class PlayerMovement : MonoBehaviour
         if (Input.GetKey(KeyCode.D))
         {
             transform.Translate(speed * Time.deltaTime, 0, 0);
+            sr.sprite = originalSprite;
         }
 
         if (Input.GetKey(KeyCode.A))
         {
             transform.Translate(-speed * Time.deltaTime, 0, 0);
+            sr.sprite = originalSprite;
         }
 
+        if (Input.GetKey(KeyCode.W))
+        {
+            transform.Translate(0, speed * Time.deltaTime, 0);
+            sr.sprite = secondSprite;
+        }
+
+        if (Input.GetKey(KeyCode.S))
+        {
+            transform.Translate(0, -speed * Time.deltaTime, 0);
+            sr.sprite = secondSprite;
+        }
 
         if (Input.GetKeyDown(KeyCode.Space))
         {
             spawnSnorlax();
         }
-
 
 
 
